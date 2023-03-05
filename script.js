9643737a013dba6a6f95a4e6ec119332b3d8615e
azilinskaite <zilinskaite.adele@gmail.com>
2023-03-05 10:52:27 +0100

    new

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-//SWIPERIS
-
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    breakpoints: {
-        768: {
-            slidesPerView: 1,
-        },
-        1000: {
-            slidesPerView: 3,
-        }
-      }
-  });
-
 //TABS
 
 var tabs = document.getElementsByClassName("tab")
---
+++ swiper.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        768: {
+            slidesPerView: 1,
+        },
+        1000: {
+            slidesPerView: 3,
+        }
+      }
+  });
